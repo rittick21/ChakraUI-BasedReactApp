3d5fb5d21cd0ae539e83a87a9925095f5bb4fa5e
Rittick <rkjob135@gmail.com>
2023-10-18 16:35:49 +0530

    assets, componentes folder are added, images are added to assets folder, header added and applied style.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 function App() {
   return <Router>
+    <Header/>
     <Routes>
       <Route/>
     </Routes>

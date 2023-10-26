e35cd9a5e221bbf431c8f8423bf7e374a75042d1
Rittick <rkjob135@gmail.com>
2023-10-26 14:25:39 +0530

    Upload component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Videos from "./components/Videos";
+import Upload from "./components/Upload";
 function App() {
   return <Router>
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/videos" element={<Videos/>}/>
+      <Route path="/upload" element={<Upload/>}/>
     </Routes>
     <Footer/>
   </Router>
